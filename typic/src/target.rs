22d6ff0e549688d370cdb3b6905482808374f5d0
Jack Wrenn <me@jswrenn.com>
2020-01-19 16:58:23 -0500

    type-level target info

---
+++ typic/src/target.rs
@@ -0,0 +1,15 @@
+//! Type-level information about the compilation target.
+
+use crate::num::*;
+
+/// The pointer width, in bytes, of the target platform.
+#[cfg(target_pointer_width = "8")]
+pub type PointerWidth = U1;
+#[cfg(target_pointer_width = "16")]
+pub type PointerWidth = U2;
+#[cfg(target_pointer_width = "32")]
+pub type PointerWidth = U4;
+#[cfg(target_pointer_width = "64")]
+pub type PointerWidth = U8;
+#[cfg(target_pointer_width = "128")]
+pub type PointerWidth = U16;
