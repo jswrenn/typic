b7c2f70fe49f63cd369a8b14cfb1e79797d67f9e
Jack Wrenn <me@jswrenn.com>
2020-01-20 17:11:00 -0500

    padding traits

---
+++ typic/src/layout/padding.rs
@@ -0,0 +1,36 @@
+use super::Layout;
+use crate::num::*;
+
+/// The amount of padding, counted in bytes, that must preceed `Self` in a
+/// compound type, where `Offset` is the index of the byte following the end of
+/// the preceeding field, and `Packed` is an unsigned integer reflecting the
+/// minimum packing of the enclosing type.
+pub trait PaddingNeededFor<Offset, Packed = <Self as Layout>::Align> {
+    type Output;
+}
+
+impl<Offset, Packed, T> PaddingNeededFor<Offset, Packed> for T
+where
+    T: Layout,
+    <T as Layout>::Align: Min<Packed>,
+    Offset: PadTo<Minimum<<T as Layout>::Align, Packed>>,
+{
+    /// In the presence of a `repr(packed(N))` modifier, this field is packed
+    /// to satisfy alignment `N` or the preferred alignment of `T`—whichever is
+    /// lower.
+    type Output = <Offset as PadTo<Minimum<<T as Layout>::Align, Packed>>>::Output;
+}
+
+/// The amount of padding bytes needed to align an item at offset `Self` to
+/// `Align`.
+pub trait PadTo<Align> {
+    type Output;
+}
+
+impl<Align, Offset> PadTo<Align> for Offset
+where
+    Offset: RoundUpTo<Align>,
+    <Offset as RoundUpTo<Align>>::Output: Sub<Offset>,
+{
+    type Output = Diff<<Offset as RoundUpTo<Align>>::Output, Offset>;
+}
--- typic/src/lib.rs
+++ typic/src/lib.rs
@@ -2,5 +2,6 @@ pub use typic_derive::repr;
 
 pub mod bytelevel;
 pub mod highlevel;
+pub mod layout;
 pub mod num;
 pub mod target;
