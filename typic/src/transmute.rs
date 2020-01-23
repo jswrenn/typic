fceab7e8765ce171dd9b17632d343bc7688abb6c
Jack Wrenn <me@jswrenn.com>
2020-01-23 13:06:07 -0500

    `Transmute` trait

---
+++ typic/src/transmute.rs
@@ -0,0 +1,22 @@
+use core::mem;
+
+mod from_layout;
+mod from_type;
+
+/// Transmute `Self` into `U`, if all possible instantiations of `Self` are
+/// valid instantiations of `U`.
+pub unsafe trait Transmute<U> {
+    #[inline(always)]
+    fn transmute(self) -> U
+    where
+        Self: Sized,
+    {
+        unsafe {
+            let to = mem::transmute_copy(&self);
+            mem::forget(self);
+            to
+        }
+    }
+}
+
+unsafe impl<T, U> Transmute<U> for T where U: from_type::FromType<T> {}
---
+++ typic/src/transmute/from_layout.rs
@@ -0,0 +1,3 @@
+/// A marker trait implemented if the layout `T` is compatible with the layout
+/// `Self`.
+pub unsafe trait FromLayout<T> {}
