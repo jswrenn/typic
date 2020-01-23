2a7ffdcdfb262f22037f473535d92203511c3393
Max Heller <heller.max1@gmail.com>
2020-01-22 20:16:19 -0500

    Rename `Align`, `Packed` to `ReprAlign`, `ReprPacked` for `IntoByteLevel` and `Type`

--- typic/src/layout/into_bytelevel/product.rs
+++ typic/src/layout/into_bytelevel/product.rs
@@ -6,89 +6,90 @@ use crate::layout::{padding::PadTo, IntoByteLevel};
 use crate::num::{self, Unsigned};
 
 #[rustfmt::skip]
-impl<Align, Packed, Offset> IntoByteLevel<Align, Packed, Offset> for highlevel::PNil
+impl<ReprAlign, ReprPacked, Offset> IntoByteLevel<ReprAlign, ReprPacked, Offset> for highlevel::PNil
 where
-    Align: Unsigned,
-    Offset: PadTo<Align> + num::Add<<Offset as PadTo<Align>>::Output>,
+    ReprAlign: Unsigned,
+    Offset: PadTo<ReprAlign> + num::Add<<Offset as PadTo<ReprAlign>>::Output>,
 
     num::Sum<
             Offset,
-            <Offset as PadTo<Align>>::Output
+            <Offset as PadTo<ReprAlign>>::Output
         >: Unsigned,
 {
     type Output =
         bytelevel::PCons<
-            PaddingSlot<<Offset as PadTo<Align>>::Output>,
+            PaddingSlot<<Offset as PadTo<ReprAlign>>::Output>,
             bytelevel::PNil
         >;
 
     type Offset =
         num::Sum<
             Offset,
-            <Offset as PadTo<Align>>::Output
+            <Offset as PadTo<ReprAlign>>::Output
         >;
 
-    type Align = Align;
+    type Align = ReprAlign;
 }
 
 #[rustfmt::skip]
-impl<Align, Packed, Offset, F, R> IntoByteLevel<Align, Packed, Offset> for highlevel::PCons<F, R>
+impl<ReprAlign, ReprPacked, Offset, F, R>
+IntoByteLevel<ReprAlign, ReprPacked, Offset> for highlevel::PCons<F, R>
 where
-    F: FieldIntoByteLevel<Packed, Offset>,
-    R: IntoByteLevel<Align, Packed, <F as FieldIntoByteLevel<Packed, Offset>>::Offset>,
+    F: FieldIntoByteLevel<ReprPacked, Offset>,
+    R: IntoByteLevel<ReprAlign, ReprPacked, <F as FieldIntoByteLevel<ReprPacked, Offset>>::Offset>,
 
-    <F as FieldIntoByteLevel<Packed, Offset>>::Output:
+    <F as FieldIntoByteLevel<ReprPacked, Offset>>::Output:
         bytelevel::Add<
             <R as IntoByteLevel<
-                Align,
-                Packed,
-                <F as FieldIntoByteLevel<Packed, Offset>>::Offset,
+                ReprAlign,
+                ReprPacked,
+                <F as FieldIntoByteLevel<ReprPacked, Offset>>::Offset,
             >>::Output,
         >,
 
-    <F as FieldIntoByteLevel<Packed, Offset>>::Align:
+    <F as FieldIntoByteLevel<ReprPacked, Offset>>::Align:
         num::Max<
             <R as IntoByteLevel<
-                Align,
-                Packed,
-                <F as FieldIntoByteLevel<Packed, Offset>>::Offset,
+                ReprAlign,
+                ReprPacked,
+                <F as FieldIntoByteLevel<ReprPacked, Offset>>::Offset,
             >>::Align,
         >,
 
     num::Maximum<
-        <F as FieldIntoByteLevel<Packed, Offset>>::Align,
+        <F as FieldIntoByteLevel<ReprPacked, Offset>>::Align,
         <R as IntoByteLevel<
-            Align,
-            Packed,
-            <F as FieldIntoByteLevel<Packed, Offset>>::Offset,
+            ReprAlign,
+            ReprPacked,
+            <F as FieldIntoByteLevel<ReprPacked, Offset>>::Offset,
         >>::Align,
     >: Unsigned,
 
 {
     type Output =
         bytelevel::Sum<
-            <F as FieldIntoByteLevel<Packed, Offset>>::Output,
+            <F as FieldIntoByteLevel<ReprPacked, Offset>>::Output,
             <R as IntoByteLevel<
-                Align,
-                Packed,
-                <F as FieldIntoByteLevel<Packed, Offset>>::Offset,
+                ReprAlign,
+                ReprPacked,
+                <F as FieldIntoByteLevel<ReprPacked, Offset>>::Offset,
             >>::Output,
         >;
 
     type Offset =
         <R as IntoByteLevel<
-            Align,
-            Packed,
-            <F as FieldIntoByteLevel<Packed, Offset>>::Offset,
+            ReprAlign,
+            ReprPacked,
+            <F as FieldIntoByteLevel<ReprPacked, Offset>>::Offset,
         >>::Offset;
 
     type Align =
         num::Maximum<
-            <F as FieldIntoByteLevel<Packed, Offset>>::Align,
+            <F as FieldIntoByteLevel<ReprPacked, Offset>>::Align,
             <R as IntoByteLevel<
-                Align,
-                Packed,
-                <F as FieldIntoByteLevel<Packed, Offset>>::Offset,
+                ReprAlign,
+                ReprPacked,
+                <F as FieldIntoByteLevel<ReprPacked, Offset>>::Offset,
             >>::Align,
         >;
 }
