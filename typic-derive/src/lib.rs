e1b7960659481352163cb4ae924fa64fe1e8c704
Jack Wrenn <me@jswrenn.com>
2020-02-01 14:48:41 -0500

    derive: parse repr

--- typic-derive/src/lib.rs
+++ typic-derive/src/lib.rs
@@ -6,9 +6,9 @@ use proc_macro::TokenStream;
 use proc_macro2::TokenStream as TokenStream2;
 use quote::*;
 use syn;
+use syn::{Meta, Attribute, Lit, NestedMeta};
 use syn::parse_macro_input;
-
-mod synstructure;
+use syn::visit::Visit;
 
 #[proc_macro_attribute]
 pub fn typicrepr(_args: TokenStream, input: TokenStream) -> TokenStream {
@@ -17,7 +17,61 @@ pub fn typicrepr(_args: TokenStream, input: TokenStream) -> TokenStream {
 
 fn impl_struct(definition: syn::ItemStruct) -> TokenStream {
     let name = &definition.ident;
-    let generics = definition.generics.clone();
+    let attrs = &definition.attrs;
+    let generics = &definition.generics;
+
+    let (impl_generics, ty_generics, where_clause) = generics.split_for_impl();
+
+    let mut repr = Repr::default();
+    attrs.into_iter().for_each(|attr| repr.visit_attribute(attr));
+
+    if let Some(Method::Transparent) = repr.method {
+        return (quote! {
+          #definition
+
+          impl #impl_generics typic::highlevel::Type
+          for #name #ty_generics #where_clause
+          {
+            #[doc(hidden)]
+            type ReprAlign =
+                <#name #ty_generics as typic::highlevel::Type>::ReprAlign;
+
+            #[doc(hidden)]
+            type ReprPacked =
+                <#name #ty_generics as typic::highlevel::Type>::ReprPacked;
+
+            #[doc(hidden)]
+            type Representation =
+                <#name #ty_generics as typic::highlevel::Type>::Representation;
+          }
+        }).into();
+    }
+
+    let repr_align =
+      repr.align.map(|n| format_ident!("U{}", n))
+        .unwrap_or(format_ident!("MinAlign"));
+
+    let repr_packed =
+      repr.align.map(|n| format_ident!("U{}", n))
+        .unwrap_or(format_ident!("MinPacked"));
+
+    // no repr
+    if let None = repr.method {
+      return (quote! {
+        #definition
+
+        impl #impl_generics typic::highlevel::Type
+        for #name #ty_generics #where_clause
+        {
+          #[doc(hidden)] type ReprAlign = typic::highlevel::#repr_align;
+          #[doc(hidden)] type ReprPacked = typic::highlevel::#repr_packed;
+          #[doc(hidden)] type Representation = Self;
+        }
+      }).into()
+    }
+
+    // otherwise, it's a C repr
+    assert_eq!(repr.method, Some(Method::C));
 
     let fields = definition
         .fields
@@ -29,14 +83,15 @@ fn impl_struct(definition: syn::ItemStruct) -> TokenStream {
         );
 
     (quote! {
-      #[repr(C)]
       #definition
 
-      impl #generics typic::hir::Type for #name #generics {
-        type Padding = typic::hir::padding::Padded;
-        type Representation = #fields;
+      impl #impl_generics typic::highlevel::Type
+      for #name #ty_generics #where_clause
+      {
+        #[doc(hidden)] type ReprAlign = typic::highlevel::#repr_align;
+        #[doc(hidden)] type ReprPacked = typic::highlevel::#repr_packed;
+        #[doc(hidden)] type Representation = #fields;
       }
-
     })
     .into()
 }
@@ -48,5 +103,98 @@ pub fn repr(args: TokenStream, input: TokenStream) -> TokenStream {
     match definition {
         syn::Item::Struct(definition) => impl_struct(definition),
         _ => unimplemented!(),
+    }
+}
+
+#[derive(Debug, Eq, PartialEq)]
+enum Method {
+    C,
+    Packed,
+    Transparent,
+}
+
+enum Size {
+    I8,
+    I16,
+    I32,
+    I64,
+    I128,
+    ISize,
+    U8,
+    U16,
+    U32,
+    U64,
+    U128,
+    USize,
+}
+
+#[derive(Default)]
+struct Repr {
+    method: Option<Method>,
+    align:  Option<u32>,
+    packed: Option<u32>,
+    size:   Option<Size>,
+}
+
+impl<'ast> Visit<'ast> for Repr {
+    fn visit_attribute(&mut self, attr: &'ast Attribute) {
+        if_chain! {
+            if let Ok(Meta::List(meta_list)) = attr.parse_meta();
+            if let Some(ident) = meta_list.path.get_ident();
+            if ident.to_string() == "repr";
+            then {
+                for meta in meta_list.nested {
+                    match meta {
+                        NestedMeta::Lit(Lit::Verbatim(lit)) => {
+                            match &lit.to_string()[..] {
+                                "c" =>
+                                    self.method = Some(Method::C),
+                                "transparent" =>
+                                    self.method = Some(Method::Transparent),
+
+                                "packed" =>
+                                    self.packed = Some(1),
+
+                                "i8"    => self.size = Some(Size::I8),
+                                "i16"   => self.size = Some(Size::I16),
+                                "i32"   => self.size = Some(Size::I32),
+                                "i64"   => self.size = Some(Size::I64),
+                                "i132"  => self.size = Some(Size::I128),
+                                "isize" => self.size = Some(Size::ISize),
+                                "u8"    => self.size = Some(Size::U8),
+                                "u16"   => self.size = Some(Size::U16),
+                                "u32"   => self.size = Some(Size::U32),
+                                "u64"   => self.size = Some(Size::U64),
+                                "u132"  => self.size = Some(Size::U128),
+                                "usize" => self.size = Some(Size::USize),
+                                _ => {},
+                            }
+                        },
+                        NestedMeta::Meta(Meta::List(meta_list)) => {
+                            if_chain! {
+                                if let Some(ident) = meta_list.path.get_ident();
+                                if meta_list.nested.len() == 1;
+                                if let Some(n) =  meta_list.nested.first();
+                                if let NestedMeta::Lit(Lit::Int(n)) = n;
+                                let ident = ident.to_string();
+                                if let Ok(n) = n.base10_parse::<u32>();
+                                then {
+                                    match &ident[..] {
+                                        "align" => {
+                                            self.align = Some(n);
+                                        },
+                                        "packed" => {
+                                             self.packed = Some(n);
+                                        }
+                                        _ => {}
+                                    }
+                                }
+                            }
+                        }
+                        _ => {},
+                    }
+                }
+            }
+        }
     }
 }
