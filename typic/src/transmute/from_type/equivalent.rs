df89b52f671c60a842d55ecddc1f56bdff6c29bf
Jack Wrenn <me@jswrenn.com>
2020-02-01 11:21:59 -0500

    fix type Equivalent and Subsumes impls

--- typic/src/transmute/from_type/equivalent.rs
+++ typic/src/transmute/from_type/equivalent.rs
@@ -1,7 +1,13 @@
-use super::from_layout;
+use crate::layout::Layout;
+use super::from_layout::Equivalent as LayoutEquivalent;
 
 /// A marker trait implemented if every instance of `T` is transmutable into
 /// an instance of `Self`.
 pub unsafe trait Equivalent<T> {}
 
-unsafe impl<T, U> Equivalent<U> for T where U: from_layout::Equivalent<T> {}
+unsafe impl<T, U> Equivalent<U> for T
+where
+    T: Layout,
+    U: Layout,
+    <U as Layout>::ByteLevel: LayoutEquivalent<<T as Layout>::ByteLevel>
+{}
--- typic/src/transmute/from_type/subsumes.rs
+++ typic/src/transmute/from_type/subsumes.rs
@@ -1,7 +1,13 @@
-use super::from_layout;
+use crate::layout::Layout;
+use super::from_layout::Subsumes as LayoutSubsumes;
 
 /// A marker trait implemented if every instance of `T` is transmutable into
 /// an instance of `Self`.
 pub unsafe trait Subsumes<T> {}
 
-unsafe impl<T, U> Subsumes<U> for T where U: from_layout::Subsumes<T> {}
+unsafe impl<T, U> Subsumes<U> for T
+where
+    T: Layout,
+    U: Layout,
+    <U as Layout>::ByteLevel: LayoutSubsumes<<T as Layout>::ByteLevel>
+{}
