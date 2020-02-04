eacb708929269e5fb59f75908bd5f4da4a52be72
Jack Wrenn <me@jswrenn.com>
2020-02-04 13:52:57 -0500

    array impls

--- typic/src/bytelevel/slot.rs
+++ typic/src/bytelevel/slot.rs
@@ -1,8 +1,10 @@
+pub mod array;
 pub mod bytes;
 pub mod reference;
 
+pub use array::Array;
 pub use bytes::Bytes;
-pub use reference::{SharedRef, UniqueRef};
+pub use reference::{Reference, SharedRef, UniqueRef};
 
 pub type PaddingSlot<S> = Bytes<bytes::kind::Uninitialized, S>;
 pub type InitializedSlot<S> = Bytes<bytes::kind::Initialized, S>;
